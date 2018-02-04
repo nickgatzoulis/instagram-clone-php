71ef80b1b030a29bad31272684dece0cdeb6a3b1
Nick Gatzoulis <30834589+nikosgatzoulis@users.noreply.github.com>
2018-02-04 19:44:01 +0000

    Implemented Ajax communication

---
+++ includes/unlike_action.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nick
+ * Date: 04/02/2018
+ * Time: 17:25
+ */
+session_start();
+
+require_once "{$_SERVER['DOCUMENT_ROOT']}/includes/db_connect.php";
+
+$pid = $_POST['pid'];
+$username = $_SESSION['username'];
+
+// Check if user liked before or not
+function unlike_action($pid, $username) {
+    global $conn;
+    $fetch_likes = $conn->query("SELECT * FROM posts_likes WHERE pid = '$pid' AND username = '$username'");
+
+    if ($fetch_likes->num_rows > 0) {
+        $conn->query("UPDATE posts_likes SET liked = 0 WHERE pid = '$pid' AND username='$username'");
+    }
+}
+
+unlike_action($pid, $username);
--- public/assets/js/like_action.js
+++ public/assets/js/like_action.js
@@ -3,14 +3,34 @@ $(document).ready(() => {
         let id = event.target.id;
         let split_id = id.split("_");
         let pid = split_id[1];
-        $(event).removeClass('far');
 
         $.ajax({
             type: "POST",
             url: "../../includes/like_action.php",
             data: {pid:pid},
             success: (data) => {
+                event.target.classList.remove('far');
+                event.target.classList.add('fas');
+                event.target.classList.remove('like');
+                event.target.classList.add('unlike');
+            }
+        });
+    });
 
+    $(".unlike").click((event) => {
+        let id = event.target.id;
+        let split_id = id.split("_");
+        let pid = split_id[1];
+
+        $.ajax({
+            type: "POST",
+            url: "../../includes/unlike_action.php",
+            data: {pid:pid},
+            success: (data) => {
+                event.target.classList.remove('fas');
+                event.target.classList.add('far');
+                event.target.classList.remove('unlike');
+                event.target.classList.add('like');
             }
         });
     });
