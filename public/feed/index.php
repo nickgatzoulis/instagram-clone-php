6c97171cf457a9b0050ffdc7809470d3c094fdce
Nikolaos Gkatzoulis <nick@tmp-110-191.essex.ac.uk>
2018-02-05 16:29:26 +0000

    Moved like/unlike action JS to a file in like_action.js

--- public/feed/index.php
+++ public/feed/index.php
@@ -57,49 +57,7 @@ if (!isset($_SESSION['logged_in'])) {
 </div>
 <?php require_once "../../includes/scripts.php"; ?>
 
-<script>
-    $(document).ready(() => {
-        $(".feed").load('../../includes/get_feed.php');
-
-        // Post Like Ajax request
-        $(".feed").on('click', '.like', (event) => {
-            let id = event.target.id;
-            let split_id = id.split("_");
-            let pid = split_id[1];
-
-            $.ajax({
-                url: '../../includes/like_action.php',
-                method: 'POST',
-                data: {pid:pid},
-                success: (data) => {
-                    event.target.classList.remove('far');
-                    event.target.classList.add('fas');
-                    event.target.classList.remove('like');
-                    event.target.classList.add('unlike');
-                }
-            });
-        });
-
-        $(".feed").on('click', '.unlike', (event) => {
-            let unlike_id = event.target.id;
-            let split_unlike_id = unlike_id.split("_");
-            let unlike_pid = split_unlike_id[1];
-
-            $.ajax({
-                url: '../../includes/unlike_action.php',
-                method: 'POST',
-                data: {unlike_pid:unlike_pid},
-                success: (data) => {
-                    event.target.classList.remove('fas');
-                    event.target.classList.add('far');
-                    event.target.classList.remove('unlike');
-                    event.target.classList.add('like');
-                }
-            });
-        });
-
-    });
-</script>
+<script src="../assets/js/like_action.js"></script>
 
 
 </body>
