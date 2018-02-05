09a6ec3e1e6989d0694d0cf942cd946fa6d775c7
Nick Gatzoulis <30834589+nikosgatzoulis@users.noreply.github.com>
2018-02-05 18:53:20 +0000

    Added comments on code

--- includes/get_comments.php
+++ includes/get_comments.php
@@ -4,12 +4,15 @@
  * User: nick
  * Date: 05/02/2018
  * Time: 18:23
+ * This PHP file fetches comments for specific PID passed via jQuery Ajax
  */
 
 session_start();
 require_once "{$_SERVER['DOCUMENT_ROOT']}/includes/db_connect.php";
 
 $pid = $_POST['pid'];
+
+// Selects all comments of that specific pid
 $comments = $conn->query("SELECT * FROM posts_comments WHERE pid = '$pid'");
 
 while ($result = $comments->fetch_assoc()) {
--- public/assets/js/get_comments.js
+++ public/assets/js/get_comments.js
@@ -1,6 +1,13 @@
 $(document).ready(() => {
+
+    // On click on .comment class...
     $(".feed").on('click', '.comment', (event) => {
+
+        // Fetch the pid of the post
         let pid = event.target.id.split("_")[1];
+
+        // Load into the appropriate post, the comments.
+        // Pass pid as POST argument via .load() method
         $(`#postid_${pid}.feed-comments`).load('../../includes/get_comments.php', {pid:pid});
     });
 });
