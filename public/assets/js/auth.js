311db7cbfe7aef0b65e9acaddaa0f34d52bfbc64
Nick Gatzoulis <30834589+nikosgatzoulis@users.noreply.github.com>
2018-02-06 01:23:27 +0000

    Minor changes to logout

--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -1,39 +1,39 @@
 $(() => {
-    // $("#login-form").validate({
-    //     rules: {
-    //         username: {
-    //             required: true,
-    //             minlength: 5
-    //         },
-    //         password: {
-    //             required: true,
-    //             minlength: 6
-    //         }
-    //     }
-    // });
-    //
-    //
-    // $("#signup-form").validate({
-    //     rules: {
-    //         username_signup: {
-    //             required: true,
-    //             minlength: 5
-    //         },
-    //         email_signup: {
-    //             required: true,
-    //             email: true
-    //         },
-    //         password_signup: {
-    //             required: true,
-    //             minlength: 6
-    //         },
-    //         password_signup_c: {
-    //             required: true,
-    //             minlength: 6,
-    //             equalTo: "#password_signup"
-    //         }
-    //     }
-    // });
+    $("#login-form").validate({
+        rules: {
+            username: {
+                required: true,
+                minlength: 5
+            },
+            password: {
+                required: true,
+                minlength: 6
+            }
+        }
+    });
+
+
+    $("#signup-form").validate({
+        rules: {
+            username_signup: {
+                required: true,
+                minlength: 5
+            },
+            email_signup: {
+                required: true,
+                email: true
+            },
+            password_signup: {
+                required: true,
+                minlength: 6
+            },
+            password_signup_c: {
+                required: true,
+                minlength: 6,
+                equalTo: "#password_signup"
+            }
+        }
+    });
 
 
     $("a[href*='#signup']").on('click', () => {
--- public/logout/index.php
+++ public/logout/index.php
@@ -1,32 +1,53 @@
 <?php require_once "../../includes/global_inc.php"; ?>
 
 <?php
-if ($_SESSION['logged_in'] == false) {
+
+
+//if (!empty($_SESSION['logged_in'])) {
+//    if ($_SESSION['logged_in'] == false) {
+//        header('Location: /public');
+//    }
+//} else {
+//    session_unset();
+//    session_destroy();
+//}
+//
+//if (empty($_SESSION['logged_in']))
+
+//if (empty($_SESSION['logged_in'])) {
+//    header('Location: /public');
+//} else {
+//    session_unset();
+//    session_destroy();
+//}
+
+if (empty($_SESSION['logged_in'])) {
     header('Location: /public');
+} else {
+    session_unset();
+    session_destroy();
 }
 
-session_unset();
-session_destroy();
 ?>
 
-<!doctype html>
-<html lang="<? echo $LNG['iso_code']; ?>">
-<head>
-    <?php require_once "../../includes/header.php"; ?>
-    <title><?php echo $LNG['logout_site_title']; ?></title>
-</head>
-<body>
-
-<div class="container">
-    <div class="row h-100v">
-        <div class="col-lg-4 align-self-center m-auto auth-box">
-            <img src="../assets/img/logo.png" height="100" class="logo">
-            <h4><? echo $LNG['logout_message'] ?></h4>
-            <p><? echo $LNG['logout_redirect'] ?><span class="countdown"></span></p>
-        </div>
-    </div>
-</div>
-<?php require_once "../../includes/scripts.php"; ?>
-<script src="../assets/js/logout.js"></script>
-</body>
-</html>
+<!--<!doctype html>-->
+<!--<html lang="--><?// echo $LNG['iso_code']; ?><!--">-->
+<!--<head>-->
+<!--    --><?php //require_once "../../includes/header.php"; ?>
+<!--    <title>--><?php //echo $LNG['logout_site_title']; ?><!--</title>-->
+<!--</head>-->
+<!--<body>-->
+<!---->
+<!--<div class="container">-->
+<!--    <div class="row h-100v">-->
+<!--        <div class="col-lg-4 align-self-center m-auto auth-box">-->
+<!--            <img src="../assets/img/logo.png" height="100" class="logo">-->
+<!--            <h4>--><?// echo $LNG['logout_message'] ?><!--</h4>-->
+<!--            <p>--><?// echo $LNG['logout_redirect'] ?><!--<span class="countdown"></span></p>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
+<?php //require_once "../../includes/scripts.php"; ?>
+<!--<script src="../assets/js/logout.js"></script>-->
+<!--</body>-->
+<!--</html>-->
